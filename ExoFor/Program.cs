38705678c6a1be5ae18112c94676c800e3e1adc3
jeremy <projeremylegendre@gmail.com>
2023-11-09 13:16:40 +0100

    until fizzbuzz project

---
+++ ExoFor/Program.cs
@@ -0,0 +1,35 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+for (int i = 0; i < 10; i+=3)
+{
+    Console.WriteLine(i);
+}
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+//string[] names = { "Alex", "Eddie", "David", "Michael" };
+// foreach (var name in names)
+// {
+//     // Can't do this:
+//     if (name == "David") name = "Sammy";
+// }
+
+//string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
---
+++ ScopeVariable/Program.cs
@@ -0,0 +1,13 @@
+﻿bool flag = true;
+if(flag) Console.WriteLine(flag);
+
+//Console.WriteLine($"Outside of code block: {value}");
+
+int value1=5;
+if(value1>0)
+{
+    
+    int value2=6;
+    value1 += value2;
+}
+Console.WriteLine(value1);
