6fa5a51716435783edb0270e93507ded05ed4c2c
jeremy <projeremylegendre@gmail.com>
2023-11-10 10:13:34 +0100

    added do while and while loop exercices

---
+++ WhileDoWhile/Program.cs
@@ -0,0 +1,16 @@
+﻿Random random = new Random();
+int current =0;
+int current2 =random.Next(1,11);
+do {
+    current = random.Next(1,11);
+    if(current>=8) continue;
+    Console.WriteLine(current);
+
+}while (current !=7);
+
+while (current2 >=3)
+{
+    Console.WriteLine(current2);
+    current2 = random.Next(1,11);
+}
+Console.WriteLine($"Last numbers {current2}");
---
+++ rpg/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+
+    Vous devez utiliser l’instruction do-while ou while comme boucle de jeu externe.
+    Le héros et le monstre commencent avec 10 points de vie.
+    Toutes les attaques ont une valeur comprise entre 1 et 10.
+    Le héros attaque en premier.
+    Afficher le nombre de points que le monstre a perdus et ses points restants.
+    Si les points de vie du monstre sont supérieurs à 0, celui-ci peut attaquer le héros.
+    Afficher le nombre de points que le héros a perdus et ses points restants.
+    Continuer cette séquence d’attaques jusqu’à ce que les points de vie du monstre ou du héros soient inférieurs ou égaux à zéro.
+    Afficher le gagnant.
+
+    sortie attendu 
+    Monster was damaged and lost 1 health and now has 9 health.
+    Hero was damaged and lost 1 health and now has 9 health.
+    Monster was damaged and lost 7 health and now has 2 health.
+    Hero was damaged and lost 6 health and now has 3 health.
+    Monster was damaged and lost 9 health and now has -7 health.
+    Hero wins!
+*/
+Random random = new Random();
+int heros = 10;
+int monsters = 10;
+int strike = 0;
+do
+    {
+    strike = random.Next(1,10);
+    monsters = monsters - strike;
+    Console.WriteLine($"Monstre a perdu {strike}pv et il lui reste {monsters}pv"); 
+  if(monsters > 0)
+  {
+    strike = random.Next(1,10);
+    heros = heros - strike;
+    Console.WriteLine($"Heros a perdu {strike}pv et il lui reste {heros}pv");
+  }else {
+    Console.WriteLine("Heros Wins");
+  }
+}while(heros>0 && monsters>0);
+if (heros <=0){
+    Console.WriteLine("Monsters Wins");
+}
